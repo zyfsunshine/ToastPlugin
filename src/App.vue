2739258a05d9c553e5324b1ad1496f320e0b07aa
zyf <1005651557@qq.com>
2020-06-24 12:23:50 +0800

    first commit

---
+++ src/App.vue
@@ -0,0 +1,32 @@
+<template>
+  <div id="app">
+    <img src="./assets/logo.png">
+    <router-view/>
+    <button @click="handleClick">点击触发</button>
+    <test-toast ref="toast"></test-toast>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'App',
+  methods:{
+    handleClick(){
+      this.$nextTick(()=>{
+        this.$refs.toast.toastPlugin('在父组件李触发调用的toast',2000)
+      })
+    }
+  }
+}
+</script>
+
+<style>
+#app {
+  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
